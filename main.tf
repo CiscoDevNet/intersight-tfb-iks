798339faec196283be0293addb95512613c0fc4f
prathjan <prathjan@cisco.com>
2021-04-25 20:01:43 -0700

    trial4

--- main.tf
+++ main.tf
@@ -69,4 +69,22 @@ data "intersight_kubernetes_sys_config_policy" "syscfg_moid" {
   name  = local.syscfg_list
 }
 
+locals {
+  organization= yamldecode(data.terraform_remote_state.global.outputs.organization)
+  ippool_list = yamldecode(data.terraform_remote_state.global.outputs.ip_pool_policy)
+  netcfg_list = yamldecode(data.terraform_remote_state.global.outputs.network_pod)
+  syscfg_list = yamldecode(data.terraform_remote_state.global.outputs.network_service)
+  clustername = yamldecode(data.terraform_remote_state.global.outputs.clustername)
+  mgmtcfgetcd = yamldecode(data.terraform_remote_state.global.outputs.mgmtcfgetcd)
+  mgmtcfglbcnt = yamldecode(data.terraform_remote_state.global.outputs.mgmtcfglbcnt)
+  mgmtcfgsshuser = yamldecode(data.terraform_remote_state.global.outputs.mgmtcfgsshuser)
+  ippoolmaster_list = yamldecode(data.terraform_remote_state.global.outputs.ippool_list)
+  ippoolworker_list = yamldecode(data.terraform_remote_state.global.outputs.ippool_list)
+  kubever_list = yamldecode(data.terraform_remote_state.global.outputs.k8s_version_name)
+  infrapolname = yamldecode(data.terraform_remote_state.global.outputs.infrapolname)
+  instancetypename = yamldecode(data.terraform_remote_state.global.outputs.instancetypename)
+  mastergrpname = yamldecode(data.terraform_remote_state.global.outputs.mastergrpname)
+  masterdesiredsize = yamldecode(data.terraform_remote_state.global.outputs.masterdesiredsize)
+  masterinfraname = yamldecode(data.terraform_remote_state.global.outputs.masterinfraname)
+}
 
