e173ae422508a58f54913283642378ae20a76263
prathjan <prathjan@cisco.com>
2021-04-25 20:09:39 -0700

    went back to all

--- main.tf
+++ main.tf
@@ -69,6 +69,129 @@ data "intersight_kubernetes_sys_config_policy" "syscfg_moid" {
   name  = local.syscfg_list
 }
 
+
+# kube cluster profiles
+resource "intersight_kubernetes_cluster_profile" "kubeprof" {
+  name = local.clustername 
+  wait_for_completion=false
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.organization_moid.results.0.moid
+  }
+  cluster_ip_pools {
+	object_type = "ippool.Pool" 
+	moid = data.intersight_ippool_pool.ippool_moid.results.0.moid
+  }
+  management_config {
+	encrypted_etcd = local.mgmtcfgetcd
+	load_balancer_count = local.mgmtcfglbcnt
+	ssh_keys = [ 
+		 var.mgmtcfgsshkeys
+	]
+	ssh_user = local.mgmtcfgsshuser
+	object_type = "kubernetes.ClusterManagementConfig" 
+  }
+  net_config {
+	moid = data.intersight_kubernetes_network_policy.netcfg_moid.results.0.moid
+	object_type = "kubernetes.NetworkPolicy" 
+  }
+
+  sys_config {
+	moid = data.intersight_kubernetes_sys_config_policy.syscfg_moid.results.0.moid
+	object_type = "kubernetes.SysConfigPolicy" 
+  }
+}
+
+
+
+# IpPool moids
+data "intersight_ippool_pool" "ippoolmaster_moid" {
+  name  = local.ippoolmaster_list
+}
+
+# IpPool moids
+data "intersight_ippool_pool" "ippoolworker_moid" {
+  name  = local.ippoolworker_list
+}
+
+# Kube version moids
+data "intersight_kubernetes_version_policy" "kubever_moid" {
+  name  = local.kubever_list
+}
+
+# Infra Config Policy
+data "intersight_kubernetes_virtual_machine_infra_config_policy" "infrapol" {
+  name  = local.infrapolname
+}
+
+# Instance Type
+data "intersight_kubernetes_virtual_machine_instance_type" "instancetype" {
+  name  = local.instancetypename
+}
+
+# Master
+resource "intersight_kubernetes_node_group_profile" "masternodegrp" {
+  name = local.mastergrpname
+  node_type = "ControlPlane"
+  desiredsize = local.masterdesiredsize
+
+  ip_pools {
+        object_type = "ippool.Pool" 
+        moid = data.intersight_ippool_pool.ippoolmaster_moid.results.0.moid
+  }
+
+
+  cluster_profile {
+        object_type = "kubernetes.ClusterProfile" 
+        moid = intersight_kubernetes_cluster_profile.kubeprof.moid
+  }
+
+
+  kubernetes_version {
+        object_type = "kubernetes.VersionPolicy" 
+        moid = data.intersight_kubernetes_version_policy.kubever_moid.results.0.moid
+  }
+
+}
+
+#Infra provider
+resource "intersight_kubernetes_virtual_machine_infrastructure_provider" "masterinfraprov" {
+	name = local.masterinfraname
+	infra_config_policy {
+		moid =  data.intersight_kubernetes_virtual_machine_infra_config_policy.infrapol.results.0.moid
+		object_type = "kubernetes.VirtualMachineInfraConfigPolicy"
+	}
+	instance_type {
+		moid =  data.intersight_kubernetes_virtual_machine_instance_type.instancetype.results.0.moid
+		object_type = "kubernetes.VirtualMachineInstanceType"
+	}
+	node_group {
+		moid = intersight_kubernetes_node_group_profile.masternodegrp.moid 
+		object_type = "kubernetes.NodeGroupProfile"
+	}
+
+}
+
+
+resource "intersight_kubernetes_cluster_profile" "kubeprofaction" {
+  depends_on = [
+        intersight_kubernetes_node_group_profile.masternodegrp
+  ]
+  action = "Deploy"
+  name = intersight_kubernetes_cluster_profile.kubeprof.name
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.organization_moid.results.0.moid 
+  }
+
+}
+
+#Wait for cluster to come up and then outpt the kubeconfig, if successful
+output "kube_config" {
+	value = intersight_kubernetes_cluster_profile.kubeprofaction.kube_config[0].kube_config
+}
+
+
 locals {
   organization= yamldecode(data.terraform_remote_state.global.outputs.organization)
   ippool_list = yamldecode(data.terraform_remote_state.global.outputs.ip_pool_policy)
@@ -87,4 +210,6 @@ locals {
   masterdesiredsize = yamldecode(data.terraform_remote_state.global.outputs.masterdesiredsize)
   masterinfraname = yamldecode(data.terraform_remote_state.global.outputs.masterinfraname)
 }
+
+
 
