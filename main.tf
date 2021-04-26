dc0a52e1fc8507507975cd7cdd8de156b3b25fcf
prathjan <prathjan@cisco.com>
2021-04-25 18:40:58 -0700

    changed to local vars

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+#get the data fro the global vars WS
+data "terraform_remote_state" "global" {
+  backend = "remote"
+  config = {
+    organization = "CiscoDevNet"
+    workspaces = {
+      name = var.globalwsname
+    }
+  }
+}
+
 # Intersight Provider Information 
 terraform {
   required_providers {
@@ -8,14 +19,30 @@ terraform {
   }
 }
 
+
+
+variable "api_key" {
+  type        = string
+  description = "API Key"
+}
+variable "secretkey" {
+  type        = string
+  description = "Secret Key"
+}
+variable "globalwsname" {
+  type        = string
+  description = "TFC WS from where to get the params"
+}
+
+
 provider "intersight" {
-  apikey        = var.api_key_id
-  secretkey = var.api_private_key
-  endpoint      = var.api_endpoint
+  apikey        = var.api_key
+  secretkey = var.secreykey
+  endpoint      = "https://intersight.com"
 }
 
 data "intersight_organization_organization" "organization_moid" {
-  name = var.organization_name
+  name = local.organization
 }
 
 output "organization_moid" {
@@ -25,23 +52,23 @@ output "organization_moid" {
 
 # IPPool moids
 data "intersight_ippool_pool" "ippool_moid" {
-  name  = var.ippool_list
+  name  = local.ippool_list
 }
 
 # Netcfg moids
 data "intersight_kubernetes_network_policy" "netcfg_moid" {
-  name  = var.netcfg_list
+  name  = local.netcfg_list
 }
 
 # Sysconfig moids
 data "intersight_kubernetes_sys_config_policy" "syscfg_moid" {
-  name  = var.syscfg_list
+  name  = local.syscfg_list
 }
 
 
 # kube cluster profiles
 resource "intersight_kubernetes_cluster_profile" "kubeprof" {
-  name = var.name 
+  name = local.clustername 
   wait_for_completion=false
   organization {
     object_type = "organization.Organization"
@@ -52,12 +79,12 @@ resource "intersight_kubernetes_cluster_profile" "kubeprof" {
 	moid = data.intersight_ippool_pool.ippool_moid.results.0.moid
   }
   management_config {
-	encrypted_etcd = var.mgmtcfgetcd
-	load_balancer_count = var.mgmtcfglbcnt
+	encrypted_etcd = local.mgmtcfgetcd
+	load_balancer_count = local.mgmtcfglbcnt
 	ssh_keys = [ 
-		 var.mgmtcfgsshkeys
+		 local.mgmtcfgsshkeys
 	]
-	ssh_user = var.mgmtcfgsshuser
+	ssh_user = local.mgmtcfgsshuser
 	object_type = "kubernetes.ClusterManagementConfig" 
   }
   net_config {
@@ -75,34 +102,34 @@ resource "intersight_kubernetes_cluster_profile" "kubeprof" {
 
 # IpPool moids
 data "intersight_ippool_pool" "ippoolmaster_moid" {
-  name  = var.ippoolmaster_list
+  name  = local.ippoolmaster_list
 }
 
 # IpPool moids
 data "intersight_ippool_pool" "ippoolworker_moid" {
-  name  = var.ippoolworker_list
+  name  = local.ippoolworker_list
 }
 
 # Kube version moids
 data "intersight_kubernetes_version_policy" "kubever_moid" {
-  name  = var.kubever_list
+  name  = local.kubever_list
 }
 
 # Infra Config Policy
 data "intersight_kubernetes_virtual_machine_infra_config_policy" "infrapol" {
-  name  = var.infrapolname
+  name  = local.infrapolname
 }
 
 # Instance Type
 data "intersight_kubernetes_virtual_machine_instance_type" "instancetype" {
-  name  = var.instancetypename
+  name  = local.instancetypename
 }
 
 # Master
 resource "intersight_kubernetes_node_group_profile" "masternodegrp" {
-  name = var.mastergrpname
+  name = local.mastergrpname
   node_type = "ControlPlane"
-  desiredsize = var.masterdesiredsize
+  desiredsize = local.masterdesiredsize
 
   ip_pools {
         object_type = "ippool.Pool" 
@@ -125,7 +152,7 @@ resource "intersight_kubernetes_node_group_profile" "masternodegrp" {
 
 #Infra provider
 resource "intersight_kubernetes_virtual_machine_infrastructure_provider" "masterinfraprov" {
-	name = var.masterinfraname
+	name = local.masterinfraname
 	infra_config_policy {
 		moid =  data.intersight_kubernetes_virtual_machine_infra_config_policy.infrapol.results.0.moid
 		object_type = "kubernetes.VirtualMachineInfraConfigPolicy"
@@ -159,3 +186,27 @@ resource "intersight_kubernetes_cluster_profile" "kubeprofaction" {
 output "kube_config" {
 	value = intersight_kubernetes_cluster_profile.kubeprofaction.kube_config[0].kube_config
 }
+
+
+locals {
+  organization= yamldecode(data.terraform_remote_state.global.outputs.organization)
+  ippool_list = yamldecode(data.terraform_remote_state.global.outputs.ippool_list)
+  netcfg_list = yamldecode(data.terraform_remote_state.global.outputs.network_pod)
+  syscfg_list = yamldecode(data.terraform_remote_state.global.outputs.network_service)
+  clustername = yamldecode(data.terraform_remote_state.global.outputs.clustername)
+  mgmtcfgetcd = yamldecode(data.terraform_remote_state.global.outputs.mgmtcfgetcd)
+  mgmtcfglbcnt = yamldecode(data.terraform_remote_state.global.outputs.mgmtcfglbcnt)
+  mgmtcfgsshkeys = yamldecode(data.terraform_remote_state.global.outputs.mgmtcfgsshkeys)
+  mgmtcfgsshuser = yamldecode(data.terraform_remote_state.global.outputs.mgmtcfgsshuser)
+  ippoolmaster_list = yamldecode(data.terraform_remote_state.global.outputs.ippool_list)
+  ippoolworker_list = yamldecode(data.terraform_remote_state.global.outputs.ippool_list)
+  kubever_list = yamldecode(data.terraform_remote_state.global.outputs.k8s_version_name)
+  infrapolname = yamldecode(data.terraform_remote_state.global.outputs.infrapolname)
+  instancetypename = yamldecode(data.terraform_remote_state.global.outputs.instancetypename)
+  mastergrpname = yamldecode(data.terraform_remote_state.global.outputs.mastergrpname)
+  masterdesiredsize = yamldecode(data.terraform_remote_state.global.outputs.masterdesiredsize)
+  masterinfraname = yamldecode(data.terraform_remote_state.global.outputs.masterinfraname)
+}
+
+
+
